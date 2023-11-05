ec1443bd07704665db744633b774a140379f7553
Ly Ngoc Tram <tramln@bsscommerce.com>
2023-11-05 20:21:09 +0700

    update

---
+++ lessonAll.py
@@ -0,0 +1,12 @@
+# !, -, and number at the beginning of a variable is not valid
+name = "Dave"
+print("This variable is " + name)
+
+# operators
+
+# 1. Assignment Operator: =
+# 2. Arithmatic Operator: +, -, *, /, //(floor division), round(24/5), %, **, +=, -=, /= (divide to decimal numbers)
+# 3. Comparison Operators: ==, ===, !=, >, <, >=, <=, not, and, or
+
+# Data types
+
