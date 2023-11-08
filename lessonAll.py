642afcb55cddb36a1eada7119677022b72fa6815
tramly <lyngoctramvns@gmail.com>
2023-11-08 17:50:44 +0700

    newUpdate

--- lessonAll.py
+++ lessonAll.py
@@ -26,3 +26,31 @@ stringFirst = "Ngoc"
 stringSecond = "Tram"
 fullname = stringFirst + " " + stringSecond
 print(fullname)
+
+#multiple lines
+multiple = '''
+This is a new line
+And then enter
+New again
+
+'''
+print(multiple)
+
+#Using single quote or double quote in string
+specialCharacters = 'I\'m saying ' + "\"To you right now\""
+print(specialCharacters)
+print(specialCharacters.lower())
+print(specialCharacters.upper())
+print(len(specialCharacters))
+
+# get a range of index value
+print(specialCharacters[1:-1]) #From the first to the last character
+print(specialCharacters[1:]) # Get index all the way to the end
+
+#Boolean data type
+myvalue = True
+wrongVal = bool(False)
+print(type(wrongVal))
+print(type(myvalue))
+
+#Numeric  data type
