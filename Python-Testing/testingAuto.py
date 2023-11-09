e6e27627c154b3535d43281e8db42a359bd38cda
tramly <lyngoctramvns@gmail.com>
2023-11-09 13:12:58 +0700

    updateSelenium

--- Python-Testing/testingAuto.py
+++ Python-Testing/testingAuto.py
@@ -1,2 +1,10 @@
 from selenium import webdriver
 
+driver = webdriver.Chrome()
+
+# Documentation https://selenium-python.readthedocs.io/getting-started.html
+# Install selenium by pip isntall selenium (or python<version> -m pip install selenium)
+# https://googlechromelabs.github.io/chrome-for-testing/#stable Chrome webdriver download here!
+
+driver.get("https://test-tram-site-4.myshopify.com/")
+element = driver.find_element_by_id('password')
