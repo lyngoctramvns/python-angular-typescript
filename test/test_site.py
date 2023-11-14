bd3f5a5b75dac0fd5d844c9bcc3968b89ccd8899
tramly <lyngoctramvns@gmail.com>
2023-11-14 22:13:54 +0700

    new

--- test/test_site.py
+++ test/test_site.py
@@ -1,15 +1,31 @@
 from seleniumbase import BaseCase
-from selenium import webdriver #Set a custom webdriver to open on Chrome
-# Alternative: from seleniumwire import webdriver
+from selenium import webdriver
+from unittest import TestCase
+from selenium.webdriver.common.by import By
+
+passwordSite = '1'
+productElement = "CardLink-template--16197650317472__product-grid-6791024148640"
+quantityBoxID = 'Quantity-template--16197650514080__main'
+productQuantity = 5
 
 
-class SiteTest(BaseCase):
+
+class SiteTest(BaseCase):	
 	def test_pass_page(self):
-		driver = webdriver.Chrome()
-		self.driver = driver
 		#open site
 		self.open("https://test-tram-site-4.myshopify.com")
 		#enter password, then click the button
-		self.find_element("password", by="id", timeout=10).send_keys("1")
-		self.click()
+		self.find_element("password", by="id", timeout=10).send_keys(passwordSite)
+		self.click('button:contains("Enter")')
 		#Check the new url
+		self.assert_url("https://test-tram-site-4.myshopify.com/")
+		self.sleep(1)
+
+	def test_catalog_page(self):
+		self.open("https://test-tram-site-4.myshopify.com")
+		#enter password, then click the button
+		self.find_element("password", by="id", timeout=10).send_keys(passwordSite)
+		self.click('button:contains("Enter")')
+		self.open("https://test-tram-site-4.myshopify.com/collections/all")
+		self.click(productElement, by="id", timeout=None)
+		self.sleep(1)
