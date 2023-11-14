1721fbcfa9e147197648ae9280633f51fa47fa43
tramly <lyngoctramvns@gmail.com>
2023-11-14 23:29:09 +0700

    update

--- test/test_site.py
+++ test/test_site.py
@@ -10,22 +10,22 @@ productQuantity = 5
 
 
 
-class SiteTest(BaseCase):	
-	def test_pass_page(self):
+class SiteTest(BaseCase):
+	def setUp(self): #setUp and super().setUp() will determine which action done before each test. The opposite is tearDown()
+		super().setUp()
+		self.driver = webdriver.Chrome()
 		#open site
 		self.open("https://test-tram-site-4.myshopify.com")
 		#enter password, then click the button
 		self.find_element("password", by="id", timeout=10).send_keys(passwordSite)
-		self.click('button:contains("Enter")')
+		self.click('button:contains("Enter")')	
+	def test_pass_page(self):
 		#Check the new url
 		self.assert_url("https://test-tram-site-4.myshopify.com/")
 		self.sleep(1)
-
 	def test_catalog_page(self):
-		self.open("https://test-tram-site-4.myshopify.com")
-		#enter password, then click the button
-		self.find_element("password", by="id", timeout=10).send_keys(passwordSite)
-		self.click('button:contains("Enter")')
 		self.open("https://test-tram-site-4.myshopify.com/collections/all")
 		self.click(productElement, by="id", timeout=None)
 		self.sleep(1)
+	def test_product_page(self):
+		self.open("")
