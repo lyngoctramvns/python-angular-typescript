abb72304fa99cff5bee22122569d5e854ab9fa80
tramly <lyngoctramvns@gmail.com>
2023-11-15 13:12:13 +0700

    update Test 3

--- test/test_site.py
+++ test/test_site.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 
 passwordSite = '1'
 productElement = "CardLink-template--16197650317472__product-grid-6791024148640"
-quantityBoxID = 'Quantity-template--16197650514080__main'
+productTitle = "2W Performance Laminated Tear Offs for EKS Brand Lucid Goggle 14 Pack"
 productQuantity = 5
 
 
@@ -27,5 +27,14 @@ class SiteTest(BaseCase):
 		self.open("https://test-tram-site-4.myshopify.com/collections/all")
 		self.click(productElement, by="id", timeout=None)
 		self.sleep(1)
+		self.assert_text(productTitle, 'h1')
 	def test_product_page(self):
-		self.open("")
+		self.open("https://test-tram-site-4.myshopify.com/products/2w-performance-laminated-tear-offs-for-eks-brand-lucid-goggle-14-pack")
+		self.clear("quantity", by="name", timeout=None)
+		self.find_element("quantity", by="name", timeout=None).send_keys(productQuantity)
+		self.click("plus", by="name", timeout=None)
+		self.click("add", by="name", timeout=None)
+		self.open("https://test-tram-site-4.myshopify.com/cart")
+		self.refresh()
+		self.assert_text("6", selector="updates[]", by = "name", timeout=None)
+
